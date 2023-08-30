d8c5b96224e630d795bbcfef60aeaa280b1dd91c
alyokaz <alyoshakaz@live.co.uk>
2023-08-30 15:21:54 +0100

    Re-enable failing test

--- src/test/java/intergration/IntegrationTest.java
+++ src/test/java/intergration/IntegrationTest.java
@@ -184,14 +184,13 @@ public class IntegrationTest {
         server.shutDown();
     }
 
-    @Disabled
     @Test
     public void pingByMultipleNodes() {
         AKTorrent server = new AKTorrent(NODE_A_PORT);
         server.startServer();
 
         Set<AKTorrent> nodes = new HashSet<>();
-        IntStream.range(0, 100000).forEach(i -> nodes.add(new AKTorrent(NODE_B_PORT + i)));
+        IntStream.range(0, 1000).forEach(i -> nodes.add(new AKTorrent(NODE_B_PORT + i)));
         nodes.forEach(node -> node.addPeer(LOCAL_HOST, NODE_A_PORT));
 
         InetSocketAddress expectedAddress = new InetSocketAddress(LOCAL_HOST, NODE_A_PORT);

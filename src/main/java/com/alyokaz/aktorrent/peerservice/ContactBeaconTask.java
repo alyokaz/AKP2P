64765f80d698013e57d2fe54e3707a1b93e57993
alyokaz <alyoshakaz@live.co.uk>
2023-09-02 12:26:26 +0100

    Refactor PeerService tasks

--- src/main/java/com/alyokaz/aktorrent/peerservice/ContactBeaconTask.java
+++ src/main/java/com/alyokaz/aktorrent/peerservice/ContactBeaconTask.java
@@ -4,25 +4,30 @@ import com.alyokaz.aktorrent.server.message.BeaconMessage;
 import com.alyokaz.aktorrent.server.message.MessageType;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
 import java.util.List;
 
-public class ContactBeaconTask extends GetPeersTaskBase {
+public class ContactBeaconTask extends AbstractPeersServiceTask {
 
     private final InetSocketAddress serverAddress;
 
-    public ContactBeaconTask(InetSocketAddress beaconAddress, List<InetSocketAddress> peers, InetSocketAddress serverAddress) {
-        super(beaconAddress, peers, (in, out) -> {
-            try {
-                out.writeObject(new BeaconMessage(MessageType.REQUEST_PEERS, serverAddress));
-                Object obj = in.readObject();
-                if(obj instanceof List<?>)
-                    peers.addAll(((List<InetSocketAddress>)obj));
-            } catch (IOException | ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-        });
+    public ContactBeaconTask(InetSocketAddress beaconAddress, List<InetSocketAddress> peers,
+                             InetSocketAddress serverAddress) {
+        super(beaconAddress, peers);
         this.serverAddress = serverAddress;
     }
 
+    @Override
+    void process(ObjectInputStream in, ObjectOutputStream out) {
+        try {
+            out.writeObject(new BeaconMessage(MessageType.REQUEST_PEERS, serverAddress));
+            Object obj = in.readObject();
+            if(obj instanceof List<?>)
+                peers.addAll(((List<InetSocketAddress>)obj));
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/com/alyokaz/aktorrent/peerservice/DiscoverPeersTask.java
+++ src/main/java/com/alyokaz/aktorrent/peerservice/DiscoverPeersTask.java
@@ -4,25 +4,29 @@ import com.alyokaz.aktorrent.server.message.Message;
 import com.alyokaz.aktorrent.server.message.MessageType;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
 import java.util.List;
 
-public class DiscoverPeersTask extends GetPeersTaskBase {
+public class DiscoverPeersTask extends AbstractPeersServiceTask {
 
     public DiscoverPeersTask(InetSocketAddress address, List<InetSocketAddress> peers) {
-        super(address, peers, (in, out) -> {
-            try {
-                out.writeObject(new Message(MessageType.REQUEST_PEERS));
-                Object obj = in.readObject();
-                List<InetSocketAddress> peerList;
-                if (obj instanceof List<?>) {
-                    peerList = (List<InetSocketAddress>) obj;
-                    peers.addAll(peerList);
-                }
-            }catch(IOException | ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        super(address, peers);
     }
 
+    @Override
+    void process(ObjectInputStream in, ObjectOutputStream out) {
+        try {
+            out.writeObject(new Message(MessageType.REQUEST_PEERS));
+            Object obj = in.readObject();
+            List<InetSocketAddress> peerList;
+            if (obj instanceof List<?>) {
+                peerList = (List<InetSocketAddress>) obj;
+                peers.addAll(peerList);
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }

70683bb93c259f2dc91cbd47fc3ded7187875308
alyokaz <alyoshakaz@live.co.uk>
2023-09-02 12:26:26 +0100

    Refactor PeerService tasks

--- src/main/java/com/alyokaz/aktorrent/peerservice/AbstractPeersServiceTask.java
+++ src/main/java/com/alyokaz/aktorrent/peerservice/AbstractPeersServiceTask.java
@@ -1,26 +1,20 @@
 package com.alyokaz.aktorrent.peerservice;
 
-import com.alyokaz.aktorrent.server.message.Message;
-import com.alyokaz.aktorrent.server.message.MessageType;
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.List;
-import java.util.function.BiConsumer;
 
-public abstract class GetPeersTaskBase implements Runnable {
+public abstract class AbstractPeersServiceTask implements Runnable {
 
     private final InetSocketAddress address;
     protected final List<InetSocketAddress> peers;
-    private final BiConsumer<ObjectInputStream, ObjectOutputStream> func;
 
-    public GetPeersTaskBase(InetSocketAddress address, List<InetSocketAddress> peers, BiConsumer<ObjectInputStream, ObjectOutputStream> func) {
+    public AbstractPeersServiceTask(InetSocketAddress address, List<InetSocketAddress> peers) {
         this.address = address;
         this.peers = peers;
-        this.func = func;
     }
 
     @Override
@@ -28,9 +22,10 @@ public abstract class GetPeersTaskBase implements Runnable {
         try (Socket socket = new Socket(address.getHostName(), address.getPort());
              ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
              ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
-            func.accept(in, out);
+            process(in, out);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+    abstract void process(ObjectInputStream in, ObjectOutputStream out);
 }

d37c8e9c4ad84c1314f3b9db9b4fd1e5fcba915f
alyokaz <alyoshakaz@live.co.uk>
2023-09-27 15:59:16 +0100

    Add documentation for PeerService

--- src/main/java/com/alyokaz/akp2p/peerservice/PeerService.java
+++ src/main/java/com/alyokaz/akp2p/peerservice/PeerService.java
@@ -18,6 +18,9 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.*;
 
+/**
+ * Service class for dealing with peer related logic,
+ */
 public class PeerService {
     private final Set<InetSocketAddress> peers = Collections.synchronizedSet(new HashSet<>());
     private final Set<InetSocketAddress> livePeers = new HashSet<>();
@@ -29,6 +32,11 @@ public class PeerService {
     public PeerService() {
     }
 
+    /**
+     * Attempts to discover new peers by contacting all known live peers
+     * and requesting the address of all known live peers that peer holds.
+     * @throws PingPeerException
+     */
     public void discoverPeers() throws PingPeerException {
         Set<Future<?>> futures = new HashSet<>();
         livePeers.forEach(address ->
@@ -43,6 +51,11 @@ public class PeerService {
         });
     }
 
+    /**
+     * Adds peer to list of live peers if it responds to ping
+     * @param address
+     * @return true if peer is added to live peers
+     */
     public synchronized boolean addPeer(InetSocketAddress address) {
         if(excluded.contains(address) || !peers.add(address))
             return false;
@@ -54,6 +67,12 @@ public class PeerService {
             return false;
     }
 
+
+    /**
+     * Attempts to ping the peer at the given address
+     * @param address the address of the peer to ping
+     * @return {@code true} if peer is successfully pinged.
+     */
     private boolean pingPeer(InetSocketAddress address) {
         try (DatagramSocket socket = new DatagramSocket()) {
             byte[] buf = PingServer.PING_PAYLOAD.getBytes();
@@ -76,6 +95,11 @@ public class PeerService {
         }
     }
 
+    /**
+     * Attempts to contact the {@code Beacon} at the supplied address.
+     * @param serverAddress the address given to register with the {@code beacon}
+     * @param beaconAddress the address of the {@code Beacon}
+     */
     public void contactBeacon(InetSocketAddress serverAddress, InetSocketAddress beaconAddress) {
        try(Socket socket  = new Socket(beaconAddress.getHostName(), beaconAddress.getPort());
            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
@@ -90,31 +114,61 @@ public class PeerService {
        }
     }
 
+    /**
+     * @return a {@code Set} of addresses of peers currently know to be live
+     */
     public Set<InetSocketAddress> getLivePeers() {
         return Collections.unmodifiableSet(this.livePeers);
     }
 
+    /**
+     * @return a {@code Set} of peer addresses not currently know to be live
+     */
     public Set<InetSocketAddress> getPeers() {
         return Collections.unmodifiableSet(this.peers);
     }
 
+    /**
+     * Removes the peers at the given address from the {@code Set} of peers
+     * currently known to be live.
+     * @param address - address of peers to be removed
+     */
     public void removeFromLivePeers(InetSocketAddress address) {
         livePeers.remove(address);
         peers.add(address);
     }
 
+    /**
+     * Adds peers at the given address to a {@code Set} of address that
+     * will not be attempted to be contacted by this instance
+     * @param address
+     * @return
+     */
     public boolean addExcluded(InetSocketAddress address) {
         return excluded.add(address);
     }
 
+    /**
+     * Removes the peer at the given address from a {@code Set} of address
+     * that will not be attempted to be contacted by this instance.
+     * @param address
+     * @return
+     */
     public boolean removeExcluded(InetSocketAddress address) {
         return excluded.remove(address);
     }
 
+    /**
+     * Sets the address of the servers for this instance.
+     * @param serverAddress - the address of the servers.
+     */
     public void setServerAddress(InetSocketAddress serverAddress) {
         this.serverAddress = serverAddress;
     }
 
+    /**
+     * @return the address for the servers of this instance.
+     */
     public InetSocketAddress getServerAddress() {
         return serverAddress;
     }
